0a260483f426f9a7a5846c8645bf01cb6b7c0d4a
Helios Tech <155942467+heliostechlabs@users.noreply.github.com>
2024-02-09 15:07:37 +0545

    Create LoginTest.js

---
+++ LoginTest.js
@@ -0,0 +1,46 @@
+const https = require('https');
+const fs = require('fs');
+
+const clientCertFilePath = 'Dab-csrs/dab-axis.p12'; // Path to your client certificate file
+const clientCertPassword = ''; // Password for your client certificate
+const url = 'https://sakshamuat.axisbank.co.in/gateway/api/v2/CRMNext/login'; // URL to which you want to make the HTTPS request
+
+// Load client certificate
+const clientCert = fs.readFileSync(clientCertFilePath);
+
+// Set options for the HTTPS request
+const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'X-IBM-Client_Id': 'cd7702c6c60795c68f2bd594a5600e1a',
+    'X-IBM-Client-Secret': 'ab638c18b5e9d598fd709deee92a2a54'
+  },
+  cert: clientCert,
+  key: clientCert,
+  passphrase: clientCertPassword
+};
+
+// Make the HTTPS request
+const req = https.request(url, options, (res) => {
+  let data = '';
+
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  res.on('end', () => {
+    console.log(data);
+  });
+});
+
+// Handle errors
+req.on('error', (error) => {
+  console.error(error);
+});
+
+// Send any request body if needed
+// req.write(JSON.stringify({}));
+
+// End the request
+req.end();
